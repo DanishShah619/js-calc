a4077e7d6568ad1e89a525aba165e00fb3a3c253
Danish Shah <shanildanshah@gmail.com>
2025-03-13 01:45:11 +0530

    minor change

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css"; // We'll add Tailwind CSS here
+import "./index.css"; 
 
 ReactDOM.render(
   <React.StrictMode>
